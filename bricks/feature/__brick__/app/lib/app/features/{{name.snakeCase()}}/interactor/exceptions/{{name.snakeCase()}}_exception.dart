ffd3abf9fbe2c26bf7580fef7bc1796b7620e636
Jacob Moura <jacobaraujo7@gmail.com>
2024-05-18 18:26:38 -0300

    Revert "Merge remote-tracking branch 'origin/main' into feature/mason-initial"

--- bricks/feature/__brick__/app/lib/app/features/{{name.snakeCase()}}/interactor/exceptions/{{name.snakeCase()}}_exception.dart
+++
@@ -1,7 +0,0 @@
-import 'package:core_module/core_module.dart';
-
-class {{name.pascalCase()}}Exception extends BaseException {
-  {{name.pascalCase()}}Exception({
-    required super.message,
-  });
-}
--- bricks/feature/__brick__/app/lib/app/features/{{name.snakeCase()}}/interactor/repositories/i_{{name.snakeCase()}}_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:coopartilhar/app/features/{{name.snakeCase()}}/interactor/entities/{{name.snakeCase()}}_entity.dart';
-import 'package:core_module/core_module.dart';
-
-abstract class I{{name.pascalCase()}}Repository {
-  Future<Output<{{name.pascalCase()}}Entity>> fetchData();
-}
